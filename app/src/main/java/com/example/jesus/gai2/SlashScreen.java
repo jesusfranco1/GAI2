48f6f1917a9384892ae3e43e538c87754fb53995
jesusfranco1 <Jesus@56-61-41-155-wireless1x.bu.edu>
2016-04-15 01:52:34 -0400

    Activity
    activity

--- app/src/main/java/com/example/jesus/gai2/SlashScreen.java
+++ app/src/main/java/com/example/jesus/gai2/SlashScreen.java
@@ -1,8 +1,8 @@
 package com.example.jesus.gai2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 /**
  * Created by Jesus on 4/5/16.
  */
-public class SlashScreen extends AppCompatActivity{
+public class SlashScreen extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
