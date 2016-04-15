2f83315c8f82ee7f38af3d79240f88986afc3c8b
jesusfranco1 <Jesus@56-61-41-155-wireless1x.bu.edu>
2016-04-14 22:39:07 -0400

    Start
    first commit

---
+++ app/src/main/java/com/example/jesus/gai2/SlashScreen.java
@@ -0,0 +1,15 @@
+package com.example.jesus.gai2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jesus on 4/5/16.
+ */
+public class SlashScreen extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
